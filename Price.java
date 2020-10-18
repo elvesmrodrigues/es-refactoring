f183b7d38f78f576761d9f69e764d1f7d7706066
Elves Rodrigues <elvesmateusrodrigues@outlook.com>
2020-10-18 08:48:22 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -4,8 +4,6 @@ public abstract class Price {
     public abstract double getCharge(int daysRented);
     
     public int getFrequentRenterPoints(int daysRented) {
-        if (daysRented > 1)
-            return 2;
         return 1;
     }
 }
